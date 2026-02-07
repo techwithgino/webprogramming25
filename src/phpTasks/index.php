e25fd49787c7b9c0904b0bc33f891045d5b1321d
ginocanoy360 <amk1004232@student.hamk.fi>
2026-02-07 14:55:04 +0200

    change index filename

--- src/phpTasks/index.php
+++ src/phpTasks/index.php
@@ -1,3 +1,7 @@
-<?php
-echo "Hello, World!";
-?>
+<?php include 'header.php'; ?>
+
+<main class="full-bg">
+    <h2>BBCAP25_Computer Applications</h2>
+</main>
+
+<?php include 'footer.php'; ?>
--- src/phpTasks/main.php
+++
@@ -1,7 +0,0 @@
-<?php include 'header.php'; ?>
-
-<main class="full-bg">
-    <h2>BBCAP25_Computer Applications</h2>
-</main>
-
-<?php include 'footer.php'; ?>
