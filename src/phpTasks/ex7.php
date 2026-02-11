5d073594dce3d1c73660d0065100bab8c403dd0c
ginocanoy360 <amk1004232@student.hamk.fi>
2026-02-11 14:01:21 +0200

    update

---
+++ src/phpTasks/ex7.php
@@ -0,0 +1,14 @@
+<?php include 'header.php'; ?>
+
+    <main>
+        <div class="container">
+            <h2>CRUD APP</h2>
+        </div>
+            <img src="img_4.jpg" alt="Exercise Image" width="60%">
+            <img src="img_5.jpg" alt="Exercise Image" width="60%">
+            <img src="img_6.jpg" alt="Exercise Image" width="60%">
+    </main>
+
+</div>
+
+<?php include 'footer.php'; ?>
