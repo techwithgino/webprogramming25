8bb6b9343a7ee914ba8b41ca73543565ab58ee35
ginocanoy360 <amk1004232@student.hamk.fi>
2026-02-11 14:34:28 +0200

    update

--- src/phpTasks/header.php
+++ src/phpTasks/header.php
@@ -9,7 +9,7 @@ $currentPage = basename($_SERVER['PHP_SELF']);
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <title>Web Programming Exercises</title>
+    <title>Web Programming Exercises (Gino Canoy)</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
