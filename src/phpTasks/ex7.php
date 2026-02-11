73994fed722fadfdd3fd039abf554ae2727cdf8c
ginocanoy360 <amk1004232@student.hamk.fi>
2026-02-11 14:11:18 +0200

    update

--- src/phpTasks/ex7.php
+++ src/phpTasks/ex7.php
@@ -4,9 +4,9 @@
         <div class="container">
             <h2>CRUD APP</h2>
         </div>
-            <img src="img_4.jpg" alt="Exercise Image" width="60%">
-            <img src="img_5.jpg" alt="Exercise Image" width="60%">
-            <img src="img_6.jpg" alt="Exercise Image" width="60%">
+            <img src="img_4.JPG" alt="Exercise Image" width="60%">
+            <img src="img_5.JPG" alt="Exercise Image" width="60%">
+            <img src="img_6.JPG" alt="Exercise Image" width="60%">
     </main>
 
 </div>
