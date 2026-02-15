9408241b0fb3978aacc38a29471d534167f6f0bb
ginocanoy360 <amk1004232@student.hamk.fi>
2026-02-15 19:28:03 +0200

    update

--- src/WebDevProject_CNSS/read.php
+++ src/WebDevProject_CNSS/read.php
@@ -1,10 +1,16 @@
 <?php
 include 'db.php';
 
-$sql = "SELECT * FROM userquery";
+// Fetch all records from userquery table
+$sql = "SELECT * FROM userquery ORDER BY sn DESC";
 $result = $conn->query($sql);
 
+if (!$result) {
+    die("Database error: " . $conn->error);
+}
+
 if ($result->num_rows > 0) {
+
     echo "<table class='table table-bordered'>
             <thead>
                 <tr>
@@ -22,21 +28,44 @@ if ($result->num_rows > 0) {
             <tbody>";
 
     while ($row = $result->fetch_assoc()) {
-        $fileLink = $row['attachment'] ? "<a href='{$row['attachment']}' target='_blank'>View</a>" : "No file";
+
+        // Escape all output to prevent XSS
+        $sn           = htmlspecialchars($row['sn']);
+        $first_name   = htmlspecialchars($row['first_name']);
+        $last_name    = htmlspecialchars($row['last_name']);
+        $email_add    = htmlspecialchars($row['email_add']);
+        $company_name = htmlspecialchars($row['company_name']);
+        $phone_num    = htmlspecialchars($row['phone_num']);
+        $query        = htmlspecialchars($row['query']);
+
+        // Handle attachment safely
+        if (!empty($row['attachment']) && file_exists($row['attachment'])) {
+            $attachmentPath = htmlspecialchars($row['attachment']);
+            $fileLink = "<a href='{$attachmentPath}' target='_blank'>View</a>";
+        } else {
+            $fileLink = "No file";
+        }
+
         echo "<tr>
-                <td>{$row['sn']}</td>
-                <td>{$row['first_name']}</td>
-                <td>{$row['last_name']}</td>
-                <td>{$row['email_add']}</td>
-                <td>{$row['company_name']}</td>
-                <td>{$row['phone_num']}</td>
-                <td>{$row['query']}</td>
-                <td>$fileLink</td>
-                <td><a href='updatesingle.php?sn={$row['sn']}' class='btn btn-sm btn-warning'>Edit/Delete</a></td>
-            </tr>";
+                <td>{$sn}</td>
+                <td>{$first_name}</td>
+                <td>{$last_name}</td>
+                <td>{$email_add}</td>
+                <td>{$company_name}</td>
+                <td>{$phone_num}</td>
+                <td>{$query}</td>
+                <td>{$fileLink}</td>
+                <td>
+                    <a href='updatesingle.php?sn={$sn}' 
+                        class='btn btn-sm btn-warning'>
+                        Edit/Delete
+                        </a>
+                    </td>
+                </tr>";
     }
 
     echo "</tbody></table>";
+
 } else {
     echo "<div class='alert alert-info'>No submissions found.</div>";
 }
